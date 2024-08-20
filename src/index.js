e66a8705bbbec56f0a3ad981f47e78593f02f056
Liza Khaletskaya <khaletskayali@gmail.com>
2024-08-20 11:11:34 +0300

    implement the towel-sort algorithm

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+   
+    if (!matrix || matrix.length === 0 ) {
+        return [];
+    }
+
+   var arrMerged = [];
+
+ 
+       for (let i = 0; i < matrix.length; i++){
+        var row = matrix[i];
+        if(i % 2 !== 0){
+        row.reverse();
+      }
+      arrMerged = arrMerged.concat(row);
+    }
+       return arrMerged;
+
 }
+
